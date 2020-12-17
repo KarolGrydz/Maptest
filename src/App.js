7704d535012eb997ee57820b8c7c5a739c295b7e
Karol Grydz <grydzkarol@gmail.com>
2020-12-17 12:39:58 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import './App.css';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Blog from './Blog';
-import BlogSingle from './BlogSingle';
+
+import Home from './scenes/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Blog from './components/Blog/Blog';
+import BlogSingle from './components/Blog/BlogSingle';
+
+import 'leaflet/dist/leaflet.css';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Switch>
-        <Route exact path="/" component={Blog} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/wyprawy/" component={Blog} />
         <Route exact path="/wyprawy/:id" component={BlogSingle} />
         {/* <Route exact path='/kim-jestesmy/' component={About} />
         <Route exact path='/kontakt/' component={Contact} />
-        <Route exact path='/wyprawy/' component={Blog} />
-        <Route exact path='/przyjaciele/' component={Friends} />
-      <Route exact path='/wyprawy/:id' component={SingleBlogPost} /> */}
+        <Route exact path='/przyjaciele/' component={Friends} /> */}
       </Switch>
+      <Footer />
     </Router>
   );
 }
